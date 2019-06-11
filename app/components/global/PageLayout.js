42941dac57a75a81cb1d2009b693d8758b2c70f8
Guido van de Riet <guidovdriet@gmail.com>
2019-06-11 21:52:59 +0200

    App init

---
+++ app/components/global/PageLayout.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { oneOfType, arrayOf, node, string } from "prop-types";
+
+// Style
+import GlobalStyle from "../../common/globalStyle";
+
+const PageLayout = ({ children }) => {
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  );
+};
+
+export default PageLayout;
+
+PageLayout.propTypes = {
+  children: oneOfType([arrayOf(node), node, string]).isRequired
+};
+
+export const Container = styled.main`
+  max-width: 1100px;
+  margin: 0 auto;
+  position: relative;
+`;
