ef35a3980894a6dbecfed9b7d5fbaeb66b07acac
Jurrian Lammerts <jurrian@lammerts.net>
2019-06-27 22:35:17 +0200

    Scaled up the iframe

--- app/pages/report.js
+++ app/pages/report.js
@@ -1,16 +1,39 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Report = () => {
   return (
-    <IFrame
-      title="graph"
-      src="https://thingspeak.com/channels/807905/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&results=1000&type=line&xaxis=Time&yaxis=ppm"
-      width="440px"
-      height="300px"
-    />
+    <Wrapper>
+      <IFrame
+        id="frame"
+        title="graph"
+        src="https://thingspeak.com/channels/807905/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&results=1000&type=line&xaxis=Time&yaxis=ppm"
+        height="100%"
+        width="100%"
+      />
+    </Wrapper>
   );
 };
 
-const IFrame = styled.iframe``;
+const IFrame = styled.iframe`
+  -ms-zoom: 2.8;
+  -moz-transform: scale(2.8);
+  -moz-transform-origin: 0 0;
+  -o-transform: scale(2.8);
+  -o-transform-origin: 0 0;
+  -webkit-transform: scale(2.8);
+  -webkit-transform-origin: 0 0;
+  #chart-container {
+    margin: 0 auto !important;
+  }
+`;
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  overflow: hidden;
+  position: absolute;
+`;
 
 export default Report;
