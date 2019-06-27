df0f3e79fb512da0a1efe822f215eef618a3606f
Jurrian Lammerts <jurrian@lammerts.net>
2019-06-27 13:18:13 +0200

    Wifi module ESP8266 test

---
+++ IOT/ThingSpeak/ThingSpeak_test/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Stads-Lab"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "Initialkey4iot"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 807905			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "7P7QKGEHDPUD2MFB"   // replace XYZ with your channel write API Key
