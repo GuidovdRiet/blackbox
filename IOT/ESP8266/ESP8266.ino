94a0564037d818e15e57ba6591f79d02e239ce3c
Jurrian Lammerts <jurrian@lammerts.net>
2019-06-27 14:36:30 +0200

    Added testing case

--- IOT/ESP8266/ESP8266.ino
+++ IOT/ESP8266/ESP8266.ino
@@ -39,6 +39,7 @@ String AP = SECRET_AP;
 String PASS = SECRET_PASS;
 String HOST = SECRET_HOST;
 String PORT = SECRET_PORT;
+String field_test = "field1";
 String field_SMOKE = "field1";
 String field_CO = "field2";
 String field_LPG = "field3";
@@ -71,16 +72,20 @@ void setup() {
   sendCommand("AT+CWJAP=\"" + AP + "\",\"" + PASS + "\"", 20, "OK");
 }
 
-//String field_SMOKE = "field1";
-//String field_CO = "field2";
-//String field_LPG = "field3";
-
-
 void loop() {
   val_smoke = MQGetGasPercentage(MQRead(MQ_PIN) / Ro, GAS_SMOKE);
   val_CO = MQGetGasPercentage(MQRead(MQ_PIN) / Ro, GAS_CO);
   val_LPG = MQGetGasPercentage(MQRead(MQ_PIN) / Ro, GAS_LPG);
-  String getData = "GET /update?api_key=" + API + "&" + field_SMOKE + "=" + String(val_smoke) + "&" + field_CO + "=" + String(val_CO) + "&" + field_LPG + "=" + String(val_LPG);
+
+  // For production uncomment next line
+//  String getData = "GET /update?api_key=" + API + "&" + field_SMOKE + "=" + String(val_smoke) + "&" + field_CO + "=" + String(val_CO) + "&" + field_LPG + "=" + String(val_LPG);
+
+  // -- Testing begin -- //
+  valSensor = getFakeSensorData();
+  String getData = "GET /update?api_key="+ API +"&"+ field_test +"="+String(valSensor);
+  // -- Testing end  -- //
+
+  
   sendCommand("AT+CIPMUX=1", 5, "OK");
   sendCommand("AT+CIPSTART=0,\"TCP\",\"" + HOST + "\"," + PORT, 15, "OK");
   sendCommand("AT+CIPSEND=0," + String(getData.length() + 4), 4, ">");
@@ -88,6 +93,11 @@ void loop() {
   delay(1500);
   countTrueCommand++;
   sendCommand("AT+CIPCLOSE=0", 5, "OK");
+}
+
+// For testing purposes
+int getFakeSensorData(){
+  return random(1000); 
 }
 
 static void chase(uint32_t c) {
@@ -227,26 +237,11 @@ float fillNeopixelBasedOnSmoke(int ppm) {
   }
 }
 
-/**************** MQResistanceCalculation **************************************
-  Input:   raw_adc - raw value read from adc, which represents the voltage
-  Output:  the calculated sensor resistance
-  Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
-         across the load resistor and its resistance, the resistance of the sensor
-         could be derived.
-**********************************************************************************/
 float MQResistanceCalculation(int raw_adc)
 {
   return ( ((float)RL_VALUE * (1023 - raw_adc) / raw_adc));
 }
 
-/*************************** MQCalibration **************************************
-  Input:   mq_pin - analog channel
-  Output:  Ro of the sensor
-  Remarks: This function assumes that the sensor is in clean air. It use
-         MQResistanceCalculation to calculates the sensor resistance in clean air
-         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
-         10, which differs slightly between different sensors.
-**********************************************************************************/
 float MQCalibration(int mq_pin)
 {
   int i;
@@ -259,18 +254,10 @@ float MQCalibration(int mq_pin)
   val = val / CALIBARAION_SAMPLE_TIMES;                 //calculate the average value
 
   val = val / RO_CLEAN_AIR_FACTOR;                      //divided by RO_CLEAN_AIR_FACTOR yields the Ro
-  //according to the chart in the datasheet
 
   return val;
 }
-/***************************  MQRead *******************************************
-  Input:   mq_pin - analog channel
-  Output:  Rs of the sensor
-  Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
-         The Rs changes as the sensor is in the different consentration of the target
-         gas. The sample times and the time interval between samples could be configured
-         by changing the definition of the macros.
-**********************************************************************************/
+
 float MQRead(int mq_pin)
 {
   int i;
@@ -286,13 +273,6 @@ float MQRead(int mq_pin)
   return rs;
 }
 
-/***************************  MQGetGasPercentage ********************************
-  Input:   rs_ro_ratio - Rs divided by Ro
-         gas_id      - target gas type
-  Output:  ppm of the target gas
-  Remarks: This function passes different curves to the MQGetPercentage function which
-         calculates the ppm (parts per million) of the target gas.
-**********************************************************************************/
 int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
 {
   if ( gas_id == GAS_LPG ) {
@@ -306,15 +286,6 @@ int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
   return 0;
 }
 
-/***************************  MQGetPercentage ********************************
-  Input:   rs_ro_ratio - Rs divided by Ro
-         pcurve      - pointer to the curve of the target gas
-  Output:  ppm of the target gas
-  Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm)
-         of the line could be derived if y(rs_ro_ratio) is provided. As it is a
-         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic
-         value.
-**********************************************************************************/
 int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
 {
   return (pow(10, ( ((log(rs_ro_ratio) - pcurve[1]) / pcurve[2]) + pcurve[0])));
