1c60f64e4e1e0a3bff7fc38883c2902d26470e04
Guido van de Riet <guidovdriet@gmail.com>
2019-06-27 23:11:37 +0200

    Bevel

--- app/pages/team.js
+++ app/pages/team.js
@@ -1,7 +1,25 @@
-import React from "react";
+import styled from "styled-components";
+import AppLayout from "../components/global/AppLayout";
 
 const team = () => {
-  return <div>team</div>;
+  return (
+    <AppLayout>
+      <People>
+        <p>Bevelvoerder</p>
+        <p>Officier</p>
+        <p>Brandonderzoeker</p>
+      </People>
+    </AppLayout>
+  );
 };
 
 export default team;
+
+const People = styled.div`
+  background-color: #2a4770;
+  color: ${({ theme }) => theme.lightPink};
+  p {
+    border-bottom: 1px solid ${({ theme }) => theme.lightPink};
+    padding: 20px;
+  }
+`;
